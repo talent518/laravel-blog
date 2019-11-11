408d0014887ee570e5bf2243fee0ac331b16aed6
ZhangBaoCai <talent518@yeah.net>
2019-11-11 17:06:25 +0800

    solve eclipse warning.

--- routes/console.php
+++ routes/console.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
+/* @var $this Illuminate\Console\Command */
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
